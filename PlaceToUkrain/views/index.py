671fa895cc85069a174f46b294f194edb51b67b8
Edgardo610 <Edgardo610@users.noreply.github.com>
2023-11-09 16:32:20 +0100

    tempo

--- PlaceToUkrain/views/index.py
+++ PlaceToUkrain/views/index.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from PlaceToUkrain.models.user import UkrUser
 from PlaceToUkrain.models.house import House
+from PlaceToUkrain.models.period import Period
 
 class IndexView(View):
 
@@ -10,4 +11,8 @@ class IndexView(View):
             return redirect('login')           
         user = UkrUser.get_user_by_id(request.session.get('user'))
         houses = House.get_houses_by_user_id(request.session.get('user'))
-        return render(request, 'index.html', {'user': user, 'houses': houses})
+        for house in houses:
+            houses_periods = (house, Period.get_periods_by_house(house))
+            print(houses_periods[0][0].id)
+        
+        return render(request, 'index.html', {'user': user, 'houses_periods': houses_periods})
